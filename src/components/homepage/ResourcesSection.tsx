fcd0ccb56cf3e4ec8c545eedc106d588256fc026
praneethpurnananda <praneeth.mandalemula@gmail.com>
2023-10-20 13:39:29 +0530

    commit

--- src/components/homepage/ResourcesSection.tsx
+++ src/components/homepage/ResourcesSection.tsx
@@ -20,12 +20,12 @@ interface Resource {
   duration: string;
 }
 
-const ALL_RESOURCES: Resource[] = [
+const DSA_RESOURCES: Resource[] = [
 
   {
     url: '/guides/basics/basics1',
     type: 'blog',
-    title: 'Python in-depth',
+    title: 'DSA-Level-0',
     description:
       'Some info regarding course and its contents',
     image:
@@ -33,15 +33,68 @@ const ALL_RESOURCES: Resource[] = [
     duration: '3 min',
   },
   {
-    url: 'https://dyte.io/blog/hls-in-depth/',
+    url: '/guides/basics/basics1',
     type: 'blog',
-    title: 'Angular basics',
+    title: 'DSA-Level-1',
     description:
       "Some info regarding course and its contents",
     image:
       'https://algorithmicaonline.com/images/course/1596570363TOP 20 ADVANCED PYTHON.png',
     duration: '10 min',
   },
+  {
+    url: '/guides/basics/basics1',
+    type: 'blog',
+    title: 'DSA-Level-2',
+    description:
+      "Some info regarding course and its contents",
+    image:
+      'https://algorithmicaonline.com/images/course/1596570363TOP 20 ADVANCED PYTHON.png',
+    duration: '10 min',
+  },
+  {
+    url: '/guides/basics/basics1',
+    type: 'blog',
+    title: 'DSA-Level-3',
+    description:
+      "Some info regarding course and its contents",
+    image:
+      'https://algorithmicaonline.com/images/course/1596570363TOP 20 ADVANCED PYTHON.png',
+    duration: '10 min',
+  },
+  {
+    url: '/guides/basics/basics1',
+    type: 'blog',
+    title: 'DSA-Level-4',
+    description:
+      "Some info regarding course and its contents",
+    image:
+      'https://algorithmicaonline.com/images/course/1596570363TOP 20 ADVANCED PYTHON.png',
+    duration: '10 min',
+  },
+];
+const AI_RESOURCES: Resource[] = [
+
+  {
+    url: '/guides/basics/basics1',
+    type: 'blog',
+    title: 'AI-ML-1',
+    description:
+      'Some info regarding course and its contents',
+    image:
+      'https://algorithmicaonline.com/images/course/1596625504LANGUAGE PYTHON AI.png',
+    duration: '3 min',
+  },
+  {
+    url: '/guides/basics/basics1',
+    type: 'blog',
+    title: 'AI-ML-2',
+    description:
+      "Some info regarding course and its contents",
+    image:
+      'https://algorithmicaonline.com/images/course/1596639591MACHINE LEARNING.png',
+    duration: '10 min',
+  }
 ];
 
 
@@ -82,14 +135,80 @@ function Resource({
   );
 }
 
-export default function ResourcesSection() {
+export default function DSAResourcesSection() {
   const [page, setPage] = useState(1);
   const [activeType, setActiveType] = useState<'all' | 'blog' | 'video'>('all');
 
   const resources =
     activeType === 'all'
-      ? ALL_RESOURCES
-      : ALL_RESOURCES.filter((r) => r.type === activeType);
+      ? DSA_RESOURCES
+      : DSA_RESOURCES.filter((r) => r.type === activeType);
+
+  const currentResources = resources.slice((page - 1) * 4, page * 4);
+
+  const pages = Math.ceil(resources.length / 4);
+
+  const nextPage = () => {
+    if (page < pages) {
+      setPage(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  return (
+    <><section className="no-underline-links my-20 px-6">
+      <div className="mx-auto max-w-7xl">
+        <div className="">
+          <h4 className="mb-2 text-2xl">Data structures & algorithms series</h4>
+
+          <p className="mb-6 text-text-400">
+            Some demo paragraph regarding data structures and algoritms
+          </p>
+        </div>
+
+
+        <div className="relative flex flex-col">
+          <div className="no-underline-links grid grid-cols-3 gap-6 md:grid-cols-2 lg:grid-cols-4">
+            {currentResources.map((resource, idx) => {
+              return <Resource {...resource} key={idx} />;
+            })}
+          </div>
+
+          <div className="my-10 ml-auto flex items-center justify-center md:my-0">
+            <button
+              onClick={prevPage}
+              className="top-1/2 -left-14 rounded-lg bg-transparent p-1 hover:bg-secondary-800 md:absolute md:-translate-y-1/2"
+            >
+              <ChevronLeftRegular className="h-6 w-6" />
+            </button>
+
+            <button
+              onClick={nextPage}
+              className="top-1/2 -right-14 rounded-lg bg-transparent p-1 hover:bg-secondary-800 md:absolute md:-translate-y-1/2"
+            >
+              <ChevronRightRegular className="h-6 w-6" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section><AIProgrammingResourcesSection /></>
+  );
+}
+
+
+export  function AIProgrammingResourcesSection() {
+  const [page, setPage] = useState(1);
+  const [activeType, setActiveType] = useState<'all' | 'blog' | 'video'>('all');
+
+  const resources =
+    activeType === 'all'
+      ? AI_RESOURCES
+      : AI_RESOURCES.filter((r) => r.type === activeType);
 
   const currentResources = resources.slice((page - 1) * 4, page * 4);
 
@@ -111,10 +230,10 @@ export default function ResourcesSection() {
     <section className="no-underline-links my-20 px-6" >
       <div className="mx-auto max-w-7xl">
         <div className="">
-        <h4 className="mb-2 text-2xl">Data structures & algorithms</h4>
+        <h4 className="mb-2 text-2xl">AI Programming series</h4>
 
         <p className="mb-6 text-text-400">
-         Some demo paragraph regarding data structures and algoritms
+         Some demo paragraph regarding AI Programming
         </p>
         </div>
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import HeroSection from '../components/homepage/HeroSection';
 import APIReferenceSection from '../components/homepage/APIReferenceSection';
 import CommunitySection from '../components/homepage/CommunitySection';
 import HomeFooter from '../components/homepage/HomeFooter';
-import ResourcesSection from '../components/homepage/ResourcesSection';
+import DSAResourcesSection from '../components/homepage/ResourcesSection';
 import HelpSection from '../components/homepage/HelpSection';
 import Head from '@docusaurus/Head';
 import GuidesAndSamples from '../components/homepage/GuidesAndSamples';
@@ -26,7 +26,8 @@ export default function Homepage() {
 
       <HeroSection />
       
-      <ResourcesSection />
+      <DSAResourcesSection />
+
  
       <HomeFooter />
       {/* 
